96606bdf04309bdce6f41fb4b33a17286ce9b90f
malinowakrew <51093851+malinowakrew@users.noreply.github.com>
2021-03-08 21:39:50 +0100

    update

--- main.py
+++ main.py
@@ -10,6 +10,24 @@ def update(popyt, podaz):
     text_elem_4.update(f"POPYT {popyt}")
     text_elem_3.update(f"PODAŻ {podaz}")
 
+def sum():
+    number = 100
+
+    suma = 0
+    for i in range(number):
+        suma += i
+
+    text_elem_4 = window['-text5-']
+    text_elem_4.update(f"{suma}")
+
+    while suma > 80:
+        suma -= 4
+
+    text_elem_4 = window['-text5-']
+    text_elem_4.update(f"{suma}")
+
+
+
 
 # Define the window's contents i.e. layout
 layout = [[sg.Text('PODAŻ:', size=(25, 1), key='-text1-', font='Helvetica 16'),
@@ -18,7 +36,9 @@ layout = [[sg.Text('PODAŻ:', size=(25, 1), key='-text1-', font='Helvetica 16'),
            sg.In(size=(25, 25), enable_events=True, key="-popyt-")],
           [sg.Button('Ok', enable_events=True, key='-Button-', font='Helvetica 16')],
         [sg.Text('POPYT', size=(25, 1), key='-text3-', font='Helvetica 16'),
-        sg.Text('POPYT', size=(25, 1), key='-text4-', font='Helvetica 16')]
+        sg.Text('POPYT', size=(25, 1), key='-text4-', font='Helvetica 16')],
+        [sg.Text('SUMA: ', size=(25, 1), key='-text5-', font='Helvetica 16'),
+         sg.Button('SHOW', enable_events=True, key='-Button2-', font='Helvetica 16')]
           ]
 
 # Create the window
@@ -31,6 +51,8 @@ while True:
         break
     if event == '-Button-':
         update(values["-popyt-"], values["-podaz-"])
+    if event == "-Button2-":
+        sum()
 
 # Close the window i.e. release resource
 window.close()
