b94fca4d919241d5c4baa19b0147216f3c50ea35
malinowakrew <51093851+malinowakrew@users.noreply.github.com>
2021-03-15 18:00:25 +0100

    gui update

--- main.py
+++ main.py
@@ -26,10 +26,10 @@ sg.theme('My New Theme')
 def update(popyt, podaz):
     text_elem_3 = window['-text3-']
     text_elem_4 = window['-text4-']
-    text_elem_4.update(f"POPYT {popyt}")
-    text_elem_3.update(f"PODAŻ {podaz}")
+    text_elem_4.update(f"POPYT: {popyt}")
+    text_elem_3.update(f"PODAŻ: {podaz}")
 
-def sum():
+def suma():
     number = 100
 
     suma = 0
@@ -46,18 +46,34 @@ def sum():
     text_elem_4.update(f"{suma}")
 
 
+def str_to_float(values):
+    return {k: float(v) for k, v in values.items()}
 
 
 # Define the window's contents i.e. layout
-layout = [[sg.Text('PODAŻ:', size=(7, 1), key='-text1-', font='Helvetica 16'),
-           sg.In(size=(25, 25), enable_events=True, key="-podaz-")],
-          [sg.Text('POPYT:', size=(7, 1), key='-text2-', font='Helvetica 16'),
-           sg.In(size=(25, 25), enable_events=True, key="-popyt-")],
+layout = [[sg.Text('PODAŻ:', size=(17, 1), key='-text1-', font='Helvetica 16'),
+           sg.In("10", size=(25, 25), enable_events=True, key="-podaz1-"),
+           sg.In("28", size=(25, 25), enable_events=True, key="-podaz2-")],
+
+            [sg.Text('POPYT:', size=(17, 1), key='-text2-', font='Helvetica 16'),
+           sg.In("20", size=(25, 25), enable_events=True, key="-popyt1-"),
+           sg.In("30", size=(25, 25), enable_events=True, key="-popyt2-")],
+
+            [sg.Text('KOSZT ZAKUPU:', size=(17, 1), key='-text3-', font='Helvetica 16'),
+           sg.In("10", size=(25, 25), enable_events=True, key="-kosztzakupu1-"),
+           sg.In("12", size=(25, 25), enable_events=True, key="-kosztzakupu2-")],
+
+            [sg.Text('CENA SPRZEDAŻY:', size=(17, 1), key='-text4-', font='Helvetica 16'),
+           sg.In("30", size=(25, 25), enable_events=True, key="-cenasprzedazy1-"),
+           sg.In("25", size=(25, 25), enable_events=True, key="-cenasprzedazy2-")],
+          [sg.Text('\nKOSZTY TRANSPORTU:', size=(25, 2), key='-text5-', font='Helvetica 16')],
+          [sg.In("8", size=(25, 25), enable_events=True, key="-kosztytransportu1-"),
+           sg.In("14", size=(25, 25), enable_events=True, key="-kosztytransportu2-")],
+          [sg.In("12", size=(25, 25), enable_events=True, key="-kosztytransportu3-"),
+           sg.In("9", size=(25, 25), enable_events=True, key="-kosztytransportu4-")],
+
           [sg.Button('Ok', enable_events=True, key='-Button-', font='Helvetica 16')],
-        [sg.Text('PODAŻ:', size=(15, 1), key='-text3-', font='Helvetica 16'),
-        sg.Text('POPYT:', size=(15, 1), key='-text4-', font='Helvetica 16')],
-        [sg.Text('SUMA: ', size=(25, 1), key='-text5-', font='Helvetica 16'),
-         sg.Button('SHOW', enable_events=True, key='-Button2-', font='Helvetica 16')]
+        [sg.Button('KONWERTER', enable_events=True, key='-Button2-', font='Helvetica 16')]
           ]
 
 # Create the window
@@ -73,7 +89,8 @@ while True:
     if event == '-Button-':
         update(values["-popyt-"], values["-podaz-"])
     if event == "-Button2-":
-        sum()
+        values = str_to_float(values)
+        sumik = sum(values.values())
 
 # Close the window i.e. release resource
 window.close()
