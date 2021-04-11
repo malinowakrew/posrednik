957dce90f78bef974640f73b368ef76eb8560dc9
zarczynsky <54021935+zarczynsky@users.noreply.github.com>
2021-04-11 11:36:25 +0200

    cycles

--- alfabeta.py
+++ alfabeta.py
@@ -1,6 +1,8 @@
 import numpy as np
 from math import isnan
 
+from cycles import *
+
 
 def alfa_beta(matrix_transport, matrix_profit):
     alfa = [0, np.nan, np.nan]
@@ -20,7 +22,24 @@ def alfa_beta(matrix_transport, matrix_profit):
     return alfa, beta
 
 
+def delta(matrix_transport, matrix_profit, betatab, alfatab):
+    alfa=np.array(alfatab)
+    beta=np.array(betatab)
+
+    delta = matrix_profit - np.stack((alfa, alfa, alfa), axis=0) - np.stack((beta, beta, beta), axis=1)
+
+    rows, columns = np.where(matrix_transport != 0.0)
+
+    for x, y in zip(rows, columns):
+        delta[x, y] = np.nan
+
+    print(delta)
+    return delta
+
+
 if __name__ == "__main__":
-    matrix_transport = np.array([[10.,  0.,  0.],[10., 18.,  0.],[ 0., 12., 38.]])
-    matrix_profit = np.array([[12.0, 1.0, 0.0],[6.0, 4.0, 0.0], [0.0, 0.0, 0.0]])
-    print(alfa_beta(matrix_transport, matrix_profit))
+    matrix_transport = np.array([[11.,  0.,  0.],[10., 18.,  0.],[ 95., 121., 38.]])
+    matrix_profit = np.array([[20.0, 1.0, 0.0],[-160.0, 14.0, 0.0], [0.0, 0.0, 0.0]])
+    alfa, beta = alfa_beta(matrix_transport, matrix_profit)
+    delt = delta(matrix_transport, matrix_profit, alfa, beta)
+    print(cycle(delt, matrix_transport))
